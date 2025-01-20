3f888893b845a062c19199da09805dc3da6880cd
PSkrabneu <skrebnev@gmail.com>
2025-01-20 15:36:45 +0100

    Test for updating multiple specific fields for PATCH added

--- src/test/java/org/tests/patch/PatchTests.java
+++ src/test/java/org/tests/patch/PatchTests.java
@@ -19,7 +19,7 @@ public class PatchTests extends BaseTest {
   private static final String PATCH_BY_ID_ENDPOINT = PATCH_ENDPOINT + "/{id}";
 
   @Test(groups = {"patch", "critical path"}, description = "Verify PATCH request updates"
-      + " specific fields of an existed post")
+      + " specific field of an existed post")
   public void testPartialUpdatePost() {
     int postId = (int) (Math.random() * 100) + 1;
     logger.info("Executing PATCH request to update post with ID: {}", postId);
@@ -39,5 +39,31 @@ public class PatchTests extends BaseTest {
         .body("title", equalTo(patchData.get("title")))
         .body("userId", notNullValue())
         .body("body", notNullValue());
+  }
+
+  @Test(groups = {"patch", "critical path"}, description = "Verify PATCH request updates with"
+      + " multiple fields of an existed post")
+  public void testMultipleFieldsUpdatePost() {
+    int postId = (int) (Math.random() * 100) + 1;
+
+    Map<String, Object> patchData = new HashMap<>();
+    patchData.put("title", "Updated title via PATCH");
+    patchData.put("body", "Updated body via PATCH");
+
+    logger.info("Executing PATCH request to update post with ID (multiple fields): {}", postId);
+
+    given()
+        .baseUri(testConfig.getBaseUrl())
+        .contentType(ContentType.JSON)
+        .pathParam("id", postId)
+        .body(patchData)
+        .when()
+        .patch(PATCH_BY_ID_ENDPOINT)
+        .then()
+        .statusCode(HttpStatus.SC_OK)
+        .body("id", equalTo(postId))
+        .body("title", equalTo(patchData.get("title")))
+        .body("body", equalTo(patchData.get("body")))
+        .body("userId", notNullValue());
   }
 }
